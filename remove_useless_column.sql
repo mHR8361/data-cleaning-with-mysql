ace4102eedaedac8fb89ab1a63df06637b7747db
Mohammad Hasan Reyhani <mohammadhsnreyhani83@gmail.com>
2025-12-23 20:48:23 +0330

    Add files via upload

---
+++ remove_useless_column.sql
@@ -0,0 +1,9 @@
+delete
+from total_layoffs
+where total_laid_off is null and percentage_laid_off is null
+;
+
+select *
+from total_layoffs
+where total_laid_off is null and percentage_laid_off is null
+;
